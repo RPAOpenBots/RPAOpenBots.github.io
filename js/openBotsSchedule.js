b08a2165f905a2cc7e6231ec5420e80cd3a71c7b
Benjamin Bobo <BBOBO@CWC.cbi.net>
2019-12-08 17:36:36 -0500

    tms beer loads weekend sched

--- js/openBotsSchedule.js
+++ js/openBotsSchedule.js
@@ -141,11 +141,15 @@ self.botSched = function botSched(){
 	//CBITDCxPBOTZ961
 	self.CBITDCxPBOTZ961 = {
 		daily:
+			["TMS - Beer Loads",0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23],
+		Saturday_and_Sunday:
 			["TMS - Beer Loads",0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
 	}
 	//CBITDCxPBOTZ962
 	self.CBITDCxPBOTZ962 = {
 		daily:
+			["TMS - Beer Loads",0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
+		Saturday_and_Sunday:
 			["TMS - Beer Loads",0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
 	}
 
