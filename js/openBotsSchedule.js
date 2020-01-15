94e3424a3e2673881de96ccfec8cc6f52b173aa9
Benjamin Bobo <BBOBO@CWC.cbi.net>
2020-01-15 10:05:15 -0500

    S4 VAT Sched

--- js/openBotsSchedule.js
+++ js/openBotsSchedule.js
@@ -132,6 +132,8 @@ self.botSched = function botSched(){
 			["Daily S&D:*8*9*10*;S4 - Glass Pro Forma:*7*22*;S4 - Lot Clearing:*23*",7,8,9,10,22,23],
 		Saturday_and_Sunday:              
 			["S4 - Glass Pro Forma:*7*22*;S4 - Lot Clearing:*23*",7,22,23],
+		MEday3:
+			["S4 - VAT",14,15,16,17],
 		MEday5:
 			["FPA - TE Report",11,12,13,14,15,16]
 	}
