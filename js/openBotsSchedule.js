d002569b32cb5ef4777f725d233073f2c7255966
Benjamin Bobo <BBOBO@CWC.cbi.net>
2020-07-09 09:40:40 -0400

    dist portal

--- js/openBotsSchedule.js
+++ js/openBotsSchedule.js
@@ -203,9 +203,7 @@ self.botSched = function botSched(){
 	//CBITDCxPBOTZ964
 	self.CBITDCxPBOTZ964 = {
 		daily:
-			["Patrol Bot - Listener",0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23],
-		Saturday_and_Sunday:
-			["Patrol Bot - Listener",0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
+			["D&A - Dist Portal",10,11],
 	}
 
 	// LIST OF BOTS - ADD HERE TO ADD A NEW BOT
