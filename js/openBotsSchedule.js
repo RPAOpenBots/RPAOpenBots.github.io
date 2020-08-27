595e3407f3efeb25215c2b43792f9a63bfbee64a
Benjamin Bobo <BBOBO@CWC.cbi.net>
2020-08-27 11:46:19 -0400

    remove pb from TDC963

--- js/openBotsSchedule.js
+++ js/openBotsSchedule.js
@@ -190,10 +190,6 @@ self.botSched = function botSched(){
 
 	//CBITDCxPBOTZ963
 	self.CBITDCxPBOTZ963 = {
-		daily:
-			["Patrol Bot - Listener",0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23],
-		Saturday_and_Sunday:
-			["Patrol Bot - Listener",0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
 	}
 	
 	//CBITDCxPBOTZ964
