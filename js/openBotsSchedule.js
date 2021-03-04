ed3fc5528e86862699c8bab7e4ddabb665874bba
Benjamin Bobo <BBOBO@CWC.cbi.net>
2021-03-04 10:32:36 -0500

    DE remove scheds

--- js/openBotsSchedule.js
+++ js/openBotsSchedule.js
@@ -25,20 +25,12 @@ self.botSched = function botSched(){
 
     //CBIGDC_PBOTZ901
     self.CBIGDCxPBOTZ901 = {
-		Monday_and_Tuesday_and_Wednesday_and_Thursday_and_Friday_and_Saturday_and_Sunday:						
-			["Manual JE",0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
     }
 
     //CBIGDC_PBOTZ902
     self.CBIGDCxPBOTZ902 = {
 		Tuesday:
 			["Sox Term",18],
-        MEdayN3_to_MEdayN2:
-			["JE Posting - Non Prod",10,11,12,13,14,15,16,17],
-        day1:
-			["Tax Compliance",7,8,9,14,15,16,17,18,19,20,21],
-        day2:
-			["Tax Compliance",3,4,5,6,7,8,9,10,11],
 		daily:
 			["S4 to JDA Int:*0*;JDA In-Transit:*23*",0,23],
 		Saturday:
@@ -51,26 +43,18 @@ self.botSched = function botSched(){
 
     //CBIGDC_PBOTZ903
     self.CBIGDCxPBOTZ903 = {
-        Monday_and_Wednesday:
-			["AP Payment",6,9,10,11],
-        Tuesday_and_Thursday_and_Friday:
-			["AP Payment",9],
 		Monday_and_Friday:
-			["IT - OIM",8],
-		daily:
-			["AP Morning Balances",5,6]
+			["IT - OIM",8]
     }
 	
 	//CBIGDC_PBOTZ904
 	self.CBIGDCxPBOTZ904 = {
 		FIRST_Wednesday:
 			["IT - AD Reconciliation",12],
-        MEday1:
-			["IC Non-Prod Rec",2,3,4,5,18,19,20,21,22],
 		MEday3:
 			["S4 - VAT",14,15,16,17],
 		daily:
-			["OMR:*6*13*;Weather Forcast:*9*;Telecom Invoices:*10*11*;Penny PO:*8*14*15*16*",6,8,9,10,11,13,14,15,16]
+			["Weather Forcast:*9*;Telecom Invoices:*10*11*;Penny PO:*8*14*15*16*",8,9,10,11,14,15,16]
 	}
 
 	//CBIGDC_PBOTZ960
@@ -107,8 +91,6 @@ self.botSched = function botSched(){
     self.CBITDCxPBOTZ901 = {
         Tuesday:
 			["Data Term Rec",9],
-        MEday1:
-			["IC Product Rec",10,11,12,13,22,23],
 		MEday3:
 			["Supplier Scorecard",14,15],
 		daily:              
@@ -118,25 +100,15 @@ self.botSched = function botSched(){
     //CBITDC_PBOTZ902
     self.CBITDCxPBOTZ902 = {
         daily:              
-			["Ariba Contact Updates:*15*16*17*;Employee Master Updates:*11*12*13*;New Hire Notification:*14*;Weekly/Monthly S&D:*8*9*;Ops Fin Dep Exp:*5*",5,8,9,11,12,13,14,15,16,17],
-		MEdayN2_to_MEday5:  
-			["FPA - Plan Forecast",6,7],
+			["New Hire Notification",14],
 		Monday_and_Tuesday_and_Wednesday_and_Thursday_and_Friday_and_Saturday_and_Sunday:
 			["Legal FET",0,1,22,23]
     }
 
     //CBITDC_PBOTZ903
     self.CBITDCxPBOTZ903 = {
-		Tuesday_and_Wednesday_and_Thursday_and_Friday:
-			["Approved Price Block",6,7,9,11,14,16,18],
-		Saturday:           
-			["Approved Price Block",6,7],
-		Monday: 
-			["Approved Price Block",9,11,14,16,18],
         daily:              
-			["VF - Freight Batch:*10*;FC- WorkFlowGen:*12*13*;Job Cost Setup:*22*23*",10,12,13,22,23],
-		MEdayN10_to_MEdayN1:
-			["Cap of Projects",2,3,4]
+			["FC- WorkFlowGen",12,13],
     }
 	
 	//CBITDC_PBOTZ904
@@ -149,14 +121,10 @@ self.botSched = function botSched(){
 			["Daily S&D:*8*9*10*;BSH:*5*6*",5,6,8,9,10],
 		Monday_and_Tuesday_and_Wednesday_and_Thursday_and_Friday_and_Saturday_and_Sunday:              
 			["S4 - Glass Pro Forma:*7*22*;S4 - Lot Clearing:*23*",7,22,23],
-		MEday5:
-			["FPA - TE Report",11,12,13,14,15,16]
 	}
 	
 	//CBITDC_PBOTZ960
 	self.CBITDCxPBOTZ960 = {
-		Monday:             
-			["SnM Program Balances",8,9],
 		Tuesday:
 			["Data Term Rec",1,2,3,4,5,6,7,8],
 		Wednesday:
@@ -191,7 +159,7 @@ self.botSched = function botSched(){
 	//CBITDCxPBOTZ964
 	self.CBITDCxPBOTZ964 = {
 		daily:
-			["D&A - Dist Portal:*9*10*11*;Supply Demand - Swaps:*12*13*14*15*16*17*18*19*20*21*",9,10,11,12,13,14,15,16,17,18,19,20,21],
+			["D&A - Dist Portal",9,10,11],
 	}
 
 	// LIST OF BOTS - ADD HERE TO ADD A NEW BOT
