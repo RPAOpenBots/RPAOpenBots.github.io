a3f4d1881d2f1af5aa94f9a478f107b8970a6c8b
Benjamin Bobo <BBOBO@CWC.cbi.net>
2021-06-16 17:04:45 -0400

    approved price block summary

--- js/openBotsSchedule.js
+++ js/openBotsSchedule.js
@@ -108,7 +108,7 @@ self.botSched = function botSched(){
 		Monday_and_Tuesday_and_Wednesday_and_Thursday_and_Friday_and_Saturday_and_Sunday:
 			["Legal FET",0,1,22,23],
 		Monday_and_Tuesday_and_Wednesday_and_Thursday_and_Friday_and_Saturday:
-			["S4 - Delivery Priority Mass Update",8,9]
+			["S4 - Delivery Priority Mass Update:*8*9*;Approved Price Block Summary:*6*9*12*15*18*21*",6,8,9,12,15,18,21]
     }
 
     //CBITDC_PBOTZ903
@@ -164,6 +164,8 @@ self.botSched = function botSched(){
 
 	//CBITDCxPBOTZ963
 	self.CBITDCxPBOTZ963 = {
+		Monday_and_Tuesday_and_Wednesday_and_Thursday_and_Friday_and_Saturday_and_Sunday:
+			["Patrol Bot - Listener",0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]
 	}
 	
 	//CBITDCxPBOTZ964
